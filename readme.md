630556e23b676b70968aea72d708eeebd79694a3
jdarling <jeremy.darling@gmail.com>
2014-02-10 17:06:50 -0600

    Initial commit

---
+++ readme.md
@@ -0,0 +1,28 @@
+wserv
+====
+
+Simple stupid static web server based on Connect and Commander.
+
+Install
+-------
+
+      npm install -g wserv
+
+Usage
+------
+
+Navigate to the folder containing your HTML/JS/CSS source and execute:
+
+```
+wserv [options]
+```
+
+Options
+=======
+
+```
+-h, --help          output usage information
+-v, --version       output the version number
+-p, --port <int>    Set port to serve on, default 8080
+-f, --folder <int>  Set directory to serve from, default C:\node projects
+```
---
+++ server.js
@@ -0,0 +1,64 @@
+var
+    path = require('path'),
+    settings = require('commander'),
+    connect = require('connect'),
+    http = require('http'),
+    server = connect(),
+    pjson = require(__dirname+'/package.json'),
+    cjson = (function(){
+      try{
+        return require(path.resolve('./wserv.json'))
+      }catch(e){
+        return {};
+      }
+    })(),
+    defaults = pjson.defaults||{},
+    webroot = path.resolve('./'),
+    webport = cjson.port||pjson.port||8080
+    ;
+
+settings
+  .version('v'+pjson.version, '-v, --version')
+  .option('-p, --port <int>', 'Set port to serve on, default '+webport, parseInt)
+  .option('-f, --folder <int>', 'Set directory to serve from, default '+webroot)
+  ;
+
+settings.unknownOption = (function(){
+  var cb = settings.unknownOption;
+  return function(opt){
+    switch(opt){
+      case('-H'):
+      case('-?'):
+        settings.help();
+        break;
+      default:
+        settings.outputHelp();
+        cb(opt);
+    }
+  };
+})();
+
+settings.parse(process.argv);
+
+if(settings.port && !isNaN(settings.port)){
+  webport = settings.port
+}
+
+if(settings.folder){
+  webroot = path.resolve('./', settings.folder);
+}
+
+server
+  .use(connect.favicon())
+  .use(connect.directory(webroot))
+  .use(connect.static(webroot))
+  .use(connect.logger('dev'))
+  ;
+
+http
+  .createServer(server)
+  .listen(webport, function(){
+    console.log('Server running on port '+webport);
+    console.log('Serving content from '+webroot);
+  })
+  ;
